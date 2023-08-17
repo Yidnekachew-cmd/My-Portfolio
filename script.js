9695b0c0c09d777fac5e276a024ef95ff85e4dae
Yidnekachew Kassahun Kibret <yamiyekn@gmail.com>
2023-08-17 18:19:31 +0300

    Add Modified readme file.

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn");
+btn.addEventListener("click", function (e) {
+  e.target.style.background = "blue";
+});
+
+// function functionYIdne() {
+//   alert("bravo!!!!!");
+// }
