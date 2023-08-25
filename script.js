01401af3759ea902b9e52bbe60d5811709acbad5
Yidne <yamiyekn@gmail.com>
2023-08-25 08:43:37 +0300

    Add images.

--- script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('btn');
-btn.addEventListener('click', (e) => {
-  e.target.style.background = 'blue';
-});
-
-// function functionYIdne() {
-//   alert("bravo!!!!!");
-// }
